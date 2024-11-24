e0a7f18bec2f2129bf904c4d1d7c45081f144955
ghanatava <68408364+ghanatava@users.noreply.github.com>
2024-11-24 18:15:39 +0530

    added grpc server

---
+++ main/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/ghanatava/learn_gRPC/target/ecommerce"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterProductInfoServer(s, &Server{})
+
+	log.Printf("Starting gRPC listener on port " + port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
